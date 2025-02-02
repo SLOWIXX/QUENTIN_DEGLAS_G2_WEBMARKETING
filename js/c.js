8554a159802cb4211d4c3b6b2c5ca7aabbb52f58
slowix <qudeglas@gmail.com>
2025-02-02 15:37:49 +0100

    Site final

---
+++ js/c.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        loop: true, 
+        autoplay: {
+            delay: 5000, 
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
